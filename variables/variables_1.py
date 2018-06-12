7fa0b2616a6c9b618670953e2657653f3b1c55a0
May Jailer <vessely.contacto@gmail.com>
2018-06-12 18:33:58 -0500

    Agregamos los archivos básicos dentro del repositorio

---
+++ variables/variables_1.py
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------------
+# Aprendemos a guardar variables por primera vez.
+# ---------------------------------------------------------------
+
+variable_a = "Mensaje para mis lectores."
+variable_b = 2442
+variable_c = 2.2344
+variable_d = True
+
+# ---------------------------------------------------------------
+# variable_a hace referencia a un dato del tipo string.
+# ---------------------------------------------------------------
+# variable_b hace referencia a un número entero.
+# ---------------------------------------------------------------
+# variable_c hace referencia a un número con punto decimal.
+# ---------------------------------------------------------------
+# variable_d hace referencia a un valor booleano.
+# ---------------------------------------------------------------
---
+++ variables/variables_2.py
@@ -0,0 +1,12 @@
+# ---------------------------------------------------------------
+# Aprendemos a convertir variables sencillas.
+# ---------------------------------------------------------------
+
+numero_entero = 2049
+texto_deseado = "Blade runner " + str(numero_entero)
+
+
+# ---------------------------------------------------------------
+# Utilizamos la concatenación de datos y la conversión
+# por medio de la función str() de un número entero
+# ---------------------------------------------------------------
