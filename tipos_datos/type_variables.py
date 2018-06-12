a7ac00ad96a5b545c5ec1a42ee13065aa3fb37d9
May Jailer <vessely.contacto@gmail.com>
2018-06-12 18:52:01 -0500

    Agregamos los archivos correspondientes a la leccion tipos de datos.

---
+++ tipos_datos/type_variables.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------------
+# Aprendemos a guardar revisar el valor de una variable.
+# ---------------------------------------------------------------
+
+mi_variable = 23982.23
+mvariable_tipo = type(mi_variable)
+
+print(mvariable_tipo)
+
+# ---------------------------------------------------------------
+# Creamos una variable y después imprimimos su tipo
+# de dato.
+# ---------------------------------------------------------------
